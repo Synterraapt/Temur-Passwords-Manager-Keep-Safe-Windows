ada16592a778e02904bb7ea8d34eb62b1b21d7c9
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:38 +0300

    PasswordModel.cs

---
+++ PasswordManager/Models/PasswordModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.API.Models
+{
+    public class PasswordModel
+    {
+        public int ID { get; set; }
+
+        public string Pass { get; set; }
+
+        public string SiteName { get; set; }
+    }
+}
