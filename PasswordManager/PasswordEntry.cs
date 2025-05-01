23980c8693af0df11eef243ee855b78bf3644d04
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:12 +0300

    PasswordEntry.cs

---
+++ PasswordManager/PasswordEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Model
+{
+    class PasswordEntry
+    {
+        public string Website { get; set; }
+        public string Username { get; set; }
+        public string EncryptedPassword { get; set; }
+        public string Category { get; set; }
+    }
+}
