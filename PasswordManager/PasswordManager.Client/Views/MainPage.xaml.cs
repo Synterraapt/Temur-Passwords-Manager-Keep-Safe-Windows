f3745ca8bdfaa79d34090bae7a4ec17d09ea4f2c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:50 +0300

    MainPage.xaml.cs

---
+++ PasswordManager/PasswordManager.Client/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using PasswordManager.API.ViewModels;
+using Plugin.Maui.Biometric;
+using UraniumUI.Pages;
+
+namespace PasswordManager.Client
+{
+    public partial class MainPage : UraniumContentPage
+    {
+        public MainPage(PasswordViewModel passwordViewModel)
+        {
+            InitializeComponent();
+            BindingContext = passwordViewModel;
+        }
+    }
+
+}
