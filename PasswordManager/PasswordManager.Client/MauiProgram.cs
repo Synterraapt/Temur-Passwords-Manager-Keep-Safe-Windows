1b12ffdf84a0848d28aa83baf712b5b4e3d87e32
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:48 +0300

    MauiProgram.cs

---
+++ PasswordManager/PasswordManager.Client/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using PasswordManager.API.ViewModels;
+using CommunityToolkit.Maui;
+using UraniumUI;
+using Plugin.Maui.Biometric;
+
+namespace PasswordManager.Client;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .UseUraniumUI()
+            .UseUraniumUIMaterial()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFontAwesomeIconFonts();
+                fonts.AddMaterialSymbolsFonts();
+            });
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<PasswordViewModel>();
+        builder.Services.AddSingleton<IBiometric>(BiometricAuthenticationService.Default);
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
