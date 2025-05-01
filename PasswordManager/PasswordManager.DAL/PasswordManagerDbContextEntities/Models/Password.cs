4197a388ecb2f38c8fce5d69b6d2d5cf4f01af4d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:55 +0300

    Password.cs

---
+++ PasswordManager/PasswordManager.DAL/PasswordManagerDbContextEntities/Models/Password.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace PasswordManager.DAL.PasswordManagerDbContextEntities.Models
+{
+    [Table("Passwords")]
+    public class Password
+    {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int ID { get; set; }
+
+        public string Pass { get; set; }
+
+        public string SiteName { get; set; }
+    }
+}
